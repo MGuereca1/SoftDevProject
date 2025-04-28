167126c32e519eb6857c25aa977cffd810b32d9a
MGuereca1 <130392333+MGuereca1@users.noreply.github.com>
2025-04-28 12:21:33 -0400

    Update PrimaryController.java
    hid password input and made a show password button

--- projectUi/PrimaryController.java
+++ projectUi/PrimaryController.java
@@ -6,13 +6,15 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
+import javafx.scene.control.ToggleButton;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class PrimaryController implements Initializable {
     // login menu fx:id's
+    
     @FXML
     private Button button_login;
 
@@ -20,18 +22,57 @@ public class PrimaryController implements Initializable {
     private TextField tf_username;
 
     @FXML
-    private TextField tf_password;
-    
+    private PasswordField pf_password;
 
+    @FXML
+    private TextField tf_passwordVisible;
+
+    @FXML
+    private ToggleButton show_bttn;
+
+    @FXML
+    void togglePasswordVisibility(ActionEvent event) {
+        if (show_bttn.isSelected()){
+            // show password (make tf_password visible, passwordfield hidden)
+            tf_passwordVisible.setVisible(true);
+            tf_passwordVisible.setManaged(true);
+            
+            pf_password.setVisible(false);
+            pf_password.setManaged(false);
+
+            show_bttn.setText("Hide");
+        }
+        else{
+            // if not selected do not set password visible
+            tf_passwordVisible.setVisible(false);
+            tf_passwordVisible.setManaged(false);
+
+            pf_password.setVisible(true);
+            pf_password.setManaged(true);
+
+            show_bttn.setText("Show");
+        }
+
+    }
+    
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        button_login.setOnAction(new EventHandler<ActionEvent>() {
+    public void initialize(final URL location, final ResourceBundle resources) {
+
+        tf_passwordVisible.setManaged(false);
+        tf_passwordVisible.setVisible(false);
+        
+        // sync password
+        tf_passwordVisible.textProperty().bindBidirectional(pf_password.textProperty());;
+        
+        // set login button action
+        button_login.setOnAction(new EventHandler<ActionEvent>() {       
             @Override
-            public void handle(ActionEvent event){
+            public void handle(final ActionEvent event){
                 // will make sure input from user is checked
-                Db_utils.UserLogin(event, tf_username.getText(),tf_password.getText());
+                Db_utils.UserLogin(event, tf_username.getText(),pf_password.getText());
             }
             
         });
     }
 }
+
